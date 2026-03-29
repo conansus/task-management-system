4b84e9bee6b87a2584d77c404210cfe6df9cee64
akhram <akhram050500@gmail.com>
2026-03-30 00:59:26 +0800

    fix: bug

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -22,7 +22,7 @@
                 <span class="badge bg-secondary">Not Assigned</span>
             @endif
         </p>
-        <p><strong>Assigned To:</strong> {{ $task->assignedUser->name ?? 'Not Assigned' }}</p>
+        <p><strong>Assigned To:</strong> {{ $task->assignedTo->name ?? 'Not Assigned' }}</p>
         <p><strong>Assigned By:</strong> {{ $task->assignedBy->name ?? '-' }}</p>
         <p><strong>Created By:</strong> {{ $task->createdBy->name ?? '-' }}</p>
         <p><strong>Due Date:</strong> {{ $task->due_date ?? '-' }}</p>
