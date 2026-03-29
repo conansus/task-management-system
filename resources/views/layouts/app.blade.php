1f078f551643e0d2fe63531074f6561628c6153d
akhram <akhram050500@gmail.com>
2026-03-30 01:06:28 +0800

    wip: ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
             ({{ auth()->user()->role === 'admin' ? 'Admin' : 'Staff' }})
         @endauth
     </title>
+    <link rel="icon" href="{{ asset('images/um_no_background.jpg') }}" type="image/jpg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
